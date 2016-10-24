35e2ec53a651868d1b1fbbda71ef1e0aaae9b6f7
RAHMANE OUSMANE <ousmanedev@gmail.com>
2016-10-24 13:14:34 +0800

    Fix author name.

--- botamp.php
+++ botamp.php
@@ -16,7 +16,7 @@
  * Plugin URI:        https://botamp.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Rahmane OUSMANE
+ * Author:            Botamp, Inc. <support@botamp.com>
  * Author URI:        support@botamp.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
