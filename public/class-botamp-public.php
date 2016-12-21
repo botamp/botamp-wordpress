34ca4eef977820bf82142b133cb76af80b70b744
Rahmane OUSMANE <ousmanedev@gmail.com>
2016-12-21 10:08:54 +0800

    Improve Botamp_Public class

--- public/class-botamp-public.php
+++ public/class-botamp-public.php
@@ -1,75 +1,27 @@
 <?php
 
 require_once plugin_dir_path( dirname( __FILE__ ) ) . 'vendor/autoload.php';
+require_once plugin_dir_path( dirname( __FILE__ ) ) . 'traits/option.php';
+require_once plugin_dir_path( dirname( __FILE__ ) ) . 'traits/botamp-client.php';
 
-/**
- * The public-facing functionality of the plugin.
- *
- * @link       support@botamp.com
- * @since      1.0.0
- *
- * @package    Botamp
- * @subpackage Botamp/public
- */
-
-/**
- * The public-facing functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
- *
- * @package    Botamp
- * @subpackage Botamp/public
- * @author     Botamp, Inc. <support@botamp.com>
- */
 class Botamp_Public {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
+	use Option;
+	use Botamp_Client;
+
 	private $plugin_name;
-
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
 	private $version;
-
-	/**
-	 * The botamp client object
-	 *
-	 * @since  1.0.0
-	 * @access private
-	 * @var    Botamp\Client         $botamp
-	 */
 	private $botamp;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string $plugin_name       The name of the plugin.
-	 * @param      string $version    The version of this plugin.
-	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-		$this->botamp = new Botamp\Client( get_option( 'botamp_api_key' ) );
-		if ( defined( 'BOTAMP_API_BASE' ) ) {
-			$this->botamp->setApiBase( BOTAMP_API_BASE );
-		}
+		$this->botamp = $this->get_botamp();
 	}
 
 	public function create_or_update_entity( $post_id ) {
-		if ( get_post_type( $post_id ) === get_option( 'botamp_post_type' )
+		if ( get_post_type( $post_id ) === $this->get_option( 'post_type' )
 				&& get_post_status( $post_id ) === 'publish' ) {
 			$params = get_fields_values( $post_id );
 			foreach ( [ 'description', 'url', 'image_url', 'title' ] as $field ) {
@@ -80,23 +32,21 @@ class Botamp_Public {
 				}
 			}
 
-			if ( ! empty( $entity_id = get_post_meta( $post_id, 'botamp_entity_id', true ) ) ) {
+			if ( ! empty( $entity_id = get_post_meta( $post_id, $this->option('entity_id'), true ) ) ) {
 				try {
 					$response = $this->botamp->entities->get( $entity_id );
 					$this->botamp->entities->update( $entity_id, $params );
 					$this->set_auth_status( 'ok' );
 				} catch (Botamp\Exceptions\NotFound $e) {
 					$response = $this->botamp->entities->create( $params );
-					update_post_meta( $post_id,
-						'botamp_entity_id',
-					$response->getBody()['data']['id']);
+					update_post_meta( $post_id, $this->option('entity_id'), $response->getBody()['data']['id']);
 				} catch (Botamp\Exceptions\Unauthorized $e) {
 					$this->set_auth_status( 'unauthorized' );
 				}
 			} else {
 				try {
 					$response = $this->botamp->entities->create( $params );
-					add_post_meta( $post_id, 'botamp_entity_id', $response->getBody()['data']['id'] );
+					add_post_meta( $post_id, $this->option('entity_id'), $response->getBody()['data']['id'] );
 					$this->set_auth_status( 'ok' );
 				} catch (Botamp\Exceptions\Unauthorized $e) {
 					$this->set_auth_status( 'unauthorized' );
@@ -106,8 +56,8 @@ class Botamp_Public {
 	}
 
 	public function delete_entity( $post_id ) {
-		if ( get_post_type( $post_id ) === get_option( 'botamp_post_type' )
-		  	&& ! empty( $entity_id = get_post_meta( $post_id, 'botamp_entity_id', true ) ) ) {
+		if ( get_post_type( $post_id ) === $this->get_option( 'post_type' )
+		  	&& ! empty( $entity_id = get_post_meta( $post_id, $this->option('entity_id'), true ) ) ) {
 			try {
 				$this->botamp->entities->delete( $entity_id );
 				$this->set_auth_status( 'ok' );
@@ -121,8 +71,8 @@ class Botamp_Public {
 		if ( ! in_array( $auth_status, [ 'ok', 'unauthorized' ] ) ) {
 			return;
 		}
-		if ( get_transient( 'botamp_auth_status' ) !== $auth_status  ) {
-			set_transient( 'botamp_auth_status', $auth_status, HOUR_IN_SECONDS );
+		if ( get_transient( $this->option('auth_status') ) !== $auth_status  ) {
+			set_transient( $this->option('auth_status'), $auth_status, HOUR_IN_SECONDS );
 		}
 	}
 
@@ -130,8 +80,8 @@ class Botamp_Public {
 		if ( ! in_array( $operation_status, [ 'ok', 'not_ok' ] ) ) {
 			return;
 		}
-		if ( get_transient( 'botamp_operation_status' ) !== $operation_status ) {
-			set_transient( 'botamp_operation_status', $operation_status, HOUR_IN_SECONDS );
+		if ( get_transient( $this->option('operation_status') ) !== $operation_status ) {
+			set_transient( $this->option('operation_status'), $operation_status, HOUR_IN_SECONDS );
 		}
 	}
 
@@ -139,7 +89,7 @@ class Botamp_Public {
 		$post = get_post( $post_id, ARRAY_A );
 		$values = [];
 		foreach ( [ 'description', 'url', 'image_url', 'title' ] as $field ) {
-			switch ( $option = get_option( 'botamp_entity_' . $field ) ) {
+			switch ( $option = $this->get_option( 'entity_' . $field ) ) {
 				case 'post_title':
 					$values[ $field ] = apply_filters( 'the_title', $post['post_title'], $post_id );
 					break;
@@ -163,50 +113,12 @@ class Botamp_Public {
 		return $values;
 	}
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Botamp_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Botamp_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/botamp-public.css', array(), $this->version, 'all' );
-
 	}
 
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Botamp_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Botamp_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/botamp-public.js', array( 'jquery' ), $this->version, false );
-
 	}
 
 }
