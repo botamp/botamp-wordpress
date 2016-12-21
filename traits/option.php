a524f6badf8f3863a0c6cfa2f009c29e4d0599da
Rahmane OUSMANE <ousmanedev@gmail.com>
2016-12-21 08:31:56 +0800

    Add trait for retrieving plugins’s options

---
+++ traits/option.php
@@ -0,0 +1,24 @@
+<?php
+
+trait Option {
+    private function option( $option_suffix ) {
+        return 'botamp_' . $option_suffix;
+    }
+
+    private function get_option( $option_suffix ) {
+        $defaults = [
+		'api_key' => '',
+		'post_type' => 'post',
+		'entity_description' => 'post_content',
+		'entity_image_url' => 'post_thumbnail_url',
+		'entity_title' => 'post_title',
+		'entity_url' => 'post_permalink',
+		];
+
+		$option = get_option( $this->option( $option_suffix ) );
+
+        return (false !== $option) ? $option : $defaults[ $option_suffix ];
+    }
+}
+
+?>
