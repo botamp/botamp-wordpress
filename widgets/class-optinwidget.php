127acc78b14b80e41e5ac9036eef94f726561d31
Rahmane OUSMANE <ousmanedev@gmail.com>
2017-03-23 22:45:10 +0800

    Allow choosing the optin to display from the widget area.

--- widgets/class-optinwidget.php
+++ widgets/class-optinwidget.php
@@ -8,16 +8,40 @@ class OptinWidget extends Wp_Widget {
 	use ProxyHelper;
 
 	public function __construct() {
-		parent::__construct( 'optin_widget', __( 'Botamp Optin Widget' , 'botamp' ) );
+		parent::__construct( 'optin_widget', __( 'Botamp Optin' , 'botamp' ) );
+	}
+
+	public function form( $instance ) {
+		$optins = $this->get_proxy( 'optin' )->all()->getBody()['data'];
+
+		$html = '<p><label>' . __( 'Optin to display:', 'botamp' )
+				. "<select class='widefat' id='{$this->get_field_id('botamp_optin')}' name='{$this->get_field_name('botamp_optin')}'>";
+
+		foreach ( $optins as $optin ) {
+			$selected_attribute = ( isset( $instance['botamp_optin'] ) && $instance['botamp_optin'] == $optin['attributes']['name'] ) ? 'selected' : '';
+			$html .= "<option value='{$optin['attributes']['name']}' {$selected_attribute}>{$optin['attributes']['name']}</option>";
+		}
+
+		echo $html .= '</select></label></p>';
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		return $new_instance;
 	}
 
 	public function widget( $args, $instance ) {
+		if ( ! isset( $instance['botamp_optin'] ) ) {
+			return;
+		}
+
 		echo $args['before_widget'];
 
-	    echo $this->get_proxy( 'optin' )
-				  ->get( $this->get_option( 'optin' ) )
+		echo $this->get_proxy( 'optin' )
+				  ->get( $instance['botamp_optin'] )
 				  ->getBody()['data']['attributes']['embed_code'];
 
 		echo $args['after_widget'];
 	}
+
+
 }
